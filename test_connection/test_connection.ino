e4f1871e92cd739def5b5a742b85107d857e5e2e
pretsb <prbharga@cs.umd.edu>
2012-11-05 20:41:39 -0500

    committing all changes

--- test_connection/test_connection.ino
+++ test_connection/test_connection.ino
@@ -14,9 +14,9 @@
  */
  #include <WiFi.h>
 
-char ssid[] = "tulane3422";     // the name of your network
-int keyIndex=1;
-char key[]="09181AF186";
+char ssid[] = "CMSC838F";     // the name of your network
+//int keyIndex=1;
+char pass[]="Hackerspace";
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
 void setup() {
@@ -26,7 +26,7 @@ void setup() {
   // attempt to connect to an open network:
   Serial.print("Attempting to connect to open network: ");
   Serial.println(ssid);
-  status = WiFi.begin(ssid,keyIndex,key);
+  status = WiFi.begin(ssid,pass);
 
   // if you're not connected, stop here:
   if ( status != WL_CONNECTED) { 
@@ -44,7 +44,7 @@ void setup() {
   
   if (client.connect(server, 8080)) {
 	    Serial.println("connected");
-	    client.println("POST /DOTSServer/authID?checkID=112613457");
+	    client.println("POST /DOTSServer/authID?checkID=111217052");
             //Serial.print("POST /DOTSServer/authID?checkID=111217052"); 
             client.println(" HTTP/1.1");
             client.println("Host: mind6.cs.umd.edu");
@@ -85,68 +85,3 @@ void loop() {
   //printCurrentNet();
 }
 
-void printWifiData() {
-  // print your WiFi shield's IP address:
-  IPAddress ip = WiFi.localIP();
-    Serial.print("IP Address: ");
-  Serial.println(ip);
-  Serial.println(ip);
-
-  // print your MAC address:
-  byte mac[6];  
-  WiFi.macAddress(mac);
-  Serial.print("MAC address: ");
-  Serial.print(mac[5],HEX);
-  Serial.print(":");
-  Serial.print(mac[4],HEX);
-  Serial.print(":");
-  Serial.print(mac[3],HEX);
-  Serial.print(":");
-  Serial.print(mac[2],HEX);
-  Serial.print(":");
-  Serial.print(mac[1],HEX);
-  Serial.print(":");
-  Serial.println(mac[0],HEX);
-
-  // print your subnet mask:
-  IPAddress subnet = WiFi.subnetMask();
-  Serial.print("NetMask: ");
-  Serial.println(subnet);
-
-  // print your gateway address:
-  IPAddress gateway = WiFi.gatewayIP();
-  Serial.print("Gateway: ");
-  Serial.println(gateway);
-}
-
-void printCurrentNet() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // print the MAC address of the router you're attached to:
-  byte bssid[6];
-  WiFi.BSSID(bssid);    
-  Serial.print("BSSID: ");
-  Serial.print(bssid[5],HEX);
-  Serial.print(":");
-  Serial.print(bssid[4],HEX);
-  Serial.print(":");
-  Serial.print(bssid[3],HEX);
-  Serial.print(":");
-  Serial.print(bssid[2],HEX);
-  Serial.print(":");
-  Serial.print(bssid[1],HEX);
-  Serial.print(":");
-  Serial.println(bssid[0],HEX);
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.println(rssi);
-
-  // print the encryption type:
-  byte encryption = WiFi.encryptionType();
-  Serial.print("Encryption Type:");
-  Serial.println(encryption,HEX);
-}
